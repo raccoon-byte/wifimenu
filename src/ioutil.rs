40e97182923ff4bd25c19eb4574abe175353a9ce
Nicolas D. <tech@nicod.me>
2024-12-04 23:31:43 -0300

    Move validate_saved_dir to ioutil

--- src/ioutil.rs
+++ src/ioutil.rs
@@ -1,8 +1,9 @@
-mod column;
-
-use anyhow::Result;
+use anyhow::{Context, Result};
 use std::io;
 use std::process;
+use crate::ioutil;
+
+mod column;
 
 pub fn display_menu(connections: &[String], title: &str) -> Option<usize> {
     if connections.is_empty() {
@@ -34,6 +35,16 @@ pub fn chmod(path: &str, mode: &str) -> Result<()> {
     process::Command::new("chmod")
         .args([mode.to_string(), path.to_string()])
         .output()?;
+
+    Ok(())
+}
+
+pub fn validate_saved_dir(path: &str) -> Result<()> {
+    if std::fs::read_dir(path).is_err() {
+        std::fs::create_dir(path)
+            .context("could not access wifi saved directory. Did you execute as root?")?;
+    }
+    ioutil::chmod(path, "400")?; // This ugly function wouldn't exist if PermissionsExt worked in OpenBSD
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::interface::Interface;
-use anyhow::{Context, Result};
+use anyhow::Result;
 
 mod errors;
 mod interface;
@@ -8,7 +8,7 @@ mod ioutil;
 const DEFAULT_SAVED_DIR: &str = "/etc/wifisaved";
 
 fn main() -> Result<()> {
-    validate_saved_dir(DEFAULT_SAVED_DIR)?;
+    ioutil::validate_saved_dir(DEFAULT_SAVED_DIR)?;
     let mut interface = Interface::build()?;
     if interface.try_saved_connection()?.is_none() {
         interface.try_new_connection()?;
@@ -17,12 +17,3 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-pub fn validate_saved_dir(path: &str) -> Result<()> {
-    if std::fs::read_dir(path).is_err() {
-        std::fs::create_dir(path)
-            .context("could not access wifi saved directory. Did you execute as root?")?;
-    }
-    ioutil::chmod(path, "400")?; // This ugly function wouldn't exist if PermissionsExt worked in OpenBSD
-
-    Ok(())
-}
